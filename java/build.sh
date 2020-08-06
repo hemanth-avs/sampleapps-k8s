a4ec382d862762c8edf0fdb20eb0bcd0436e562c
Hemanth AVS <hemanth.avs@gmail.com>
2020-08-06 15:13:45 -0500

    Added Java repo

---
+++ java/build.sh
@@ -0,0 +1,12 @@
+rm -rf jre8u265
+
+FILE=bellsoft-jre8u265+1-linux-amd64.tar.gz
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+    tar xf bellsoft-jre8u265+1-linux-amd64.tar.gz
+    docker build -t java:latest .
+    docker run  java:latest1 /bin/bash -c "java -version"
+else 
+    echo "$FILE does not exist. Please copy file from Tanzu network"
+fi
+
