6662a17332f9d5ae287371388541b9783d9164f0
Hemanth AVS <hemanth.avs@gmail.com>
2020-08-06 15:19:45 -0500

    updated image name

--- java/build.sh
+++ java/build.sh
@@ -5,8 +5,7 @@ if [ -f "$FILE" ]; then
     echo "$FILE exists."
     tar xf bellsoft-jre8u265+1-linux-amd64.tar.gz
     docker build -t java:latest .
-    docker run  java:latest1 /bin/bash -c "java -version"
+    docker run  java:latest /bin/bash -c "java -version"
 else 
     echo "$FILE does not exist. Please copy file from Tanzu network"
 fi
-
