46cec86087a1a76136ad4518f73156f9be05bb23
Hemanth AVS <hemanth.avs@gmail.com>
2020-10-13 18:48:52 -0500

    updated changes

--- java/build.sh
+++ java/build.sh
@@ -1,12 +1,12 @@
-rm -rf jdk8u265
-
 export tag=v4
+
 FILE=bellsoft-jdk8u265+1-linux-amd64.tar.gz
+
 if [ -f "$FILE" ]; then
     echo "$FILE exists."
-    tar xf bellsoft-jdk8u265+1-linux-amd64.tar.gz
-    docker build -f Dockerfile-uib -t java:$tag .
+    docker build -f Dockerfile-uib-v1 -t java:$tag .
     docker run java:$tag /bin/bash -c "java -version"
+    docker images java:$tag
 else 
     echo "$FILE does not exist. Please copy file from Tanzu network"
 fi
