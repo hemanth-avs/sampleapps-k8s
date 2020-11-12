b5fbd0688566b9f868e1b04dd5c4ca372097097e
Hemanth AVS <hemanth.avs@gmail.com>
2020-11-12 11:36:55 -0600

    update to jdk 11

--- java/build.sh
+++ java/build.sh
@@ -1,10 +1,10 @@
-export tag=v4
+export tag=v5
 
-FILE=bellsoft-jdk8u265+1-linux-amd64.tar.gz
+FILE=bellsoft-jdk11.0.9+12-linux-amd64.tar.gz
 
 if [ -f "$FILE" ]; then
     echo "$FILE exists."
-    docker build -f Dockerfile-uib-v1 -t java:$tag .
+    docker build -f Dockerfile-uib -t java:$tag .
     docker run java:$tag /bin/bash -c "java -version"
     docker images java:$tag
 else 
